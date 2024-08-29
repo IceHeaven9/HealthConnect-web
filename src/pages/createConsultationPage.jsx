319f406dd64fd558d6ae711928d02ea2c366e9e5
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-08-29 15:07:47 +0200

    router y auth constext

---
+++ src/pages/createConsultationPage.jsx
@@ -0,0 +1,3 @@
+export const CreateConsultationPage = () => {
+	return <div>Create consultation</div>;
+};
---
+++ src/pages/validateEmailPage.jsx
@@ -0,0 +1,3 @@
+export const ValidateEmailPage = () => {
+	return <div>validateEmailPage</div>;
+};
