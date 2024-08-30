5cf1b603417b9d94575853aa894f65a252f531ca
ana maria forero <anamariaforero3115@gmail.com>
2024-08-30 18:31:56 +0200

    LoginPage

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,3 +1,10 @@
 export const LoginPage = () => {
-	return <div>Login</div>;
+	return 
+	<div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full bg-white p-8 border border-gray-300 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+         
+      </div>
+    </div>
 };
+
