01d0f57458d517572b72bc6df0f9c2226c6d2e7c
Ivan Sanchez <mourazos1995@gmail.com>
2024-09-01 17:05:25 +0200

    changes-LoginPage

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,10 +1,11 @@
-export const LoginPage = () => {
-	return 
-	<div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="max-w-md w-full bg-white p-8 border border-gray-300 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-         
-      </div>
-    </div>
-};
+import { LoginForm } from "../components/forms/LoginForm";
 
+export const LoginPage = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <div>Header</div>
+      <LoginForm />
+      <div>Footer</div>
+    </div>
+  );
+};
