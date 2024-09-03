db1c3ecba2e9663896490c587fa99efcdadf2aeb
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-09-03 18:37:58 +0200

    x

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,3 +1,23 @@
 export const LoginForm = () => {
-  return <>LoginForm</>;
+  return( <>    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+  
+    <div className="max-w-md w-full bg-white p-8 border border-gray-300 rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+  
+      
+        <div>
+        <input/>
+        </div>
+        
+        
+        <div>
+         <input/>
+        </div>
+        <button type="submit" className="w-full bg-indigo-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Login
+        </button>
+      
+    </div>
+  </div>
+  </>);
 };
