65311378904024fda9db13ef2d4908197d8f46fe
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-09-04 03:07:30 +0200

    loginPage

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,23 +1,146 @@
+import { useState, useContext } from 'react';
+import { AuthContext } from './../../contexts/authContext';
+import { useNavigate } from 'react-router-dom';
+
 export const LoginForm = () => {
-  return( <>    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-  
-    <div className="max-w-md w-full bg-white p-8 border border-gray-300 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-  
-      
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const { onLogin } = useContext(AuthContext);
+
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      email: email,
+      password: password
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("http://localhost:3000/login", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.token) {
+          onLogin(result.token);
+          navigate('/');
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="flex flex-col w-full md:w-1/2 xl:w-2/5 2xl:w-2/5 3xl:w-1/3 mx-auto p-8 md:p-10 2xl:p-12 3xl:p-14 bg-[#ffffff] rounded-2xl shadow-xl">
+      <div className="flex flex-row gap-3 pb-4">
         <div>
-        <input/>
+          <img src="/public/images/Perfil_healthConnect-Photoroom.png" width="50" alt="Logo" />
         </div>
-        
-        
-        <div>
-         <input/>
+        <h1 className="text-3xl font-bold text-[#4B5563] my-auto">HealthConnect</h1>
+      </div>
+      <div className="text-sm font-light text-[#6B7280] pb-8">
+        Introduce tu correo electronico y contraseña para iniciar sesion
+      </div>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <div className="pb-2">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-[#111827]">
+            Email
+          </label>
+          <div className="relative text-gray-400">
+            <span className="absolute inset-y-0 left-0 flex items-center p-1 pl-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-mail"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </svg>
+            </span>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="pl-12 mb-2 bg-gray-50 text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4"
+              placeholder="name@company.com"
+              autoComplete="off"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
         </div>
-        <button type="submit" className="w-full bg-indigo-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          Login
+        <div className="pb-6">
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-[#111827]">
+            Password
+          </label>
+          <div className="relative text-gray-400">
+            <span className="absolute inset-y-0 left-0 flex items-center p-1 pl-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-square-asterisk"
+              >
+                <rect width="18" height="18" x="3" y="3" rx="2"></rect>
+                <path d="M12 8v8"></path>
+                <path d="m8.5 14 7-4"></path>
+                <path d="m8.5 10 7 4"></path>
+              </svg>
+            </span>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••••"
+              className="pl-12 mb-2 bg-gray-50 text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4"
+              autoComplete="new-password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="w-full text-[#FFFFFF] bg-[#628eff] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-6"
+        >
+          Continuar
         </button>
-      
+        <div className="text-sm font-light text-[#6B7280] text-center">
+          No tienes una cuenta?{" "}
+          <a href="/register" className="font-medium text-[#628eff] hover:underline">
+            Registrate
+          </a>
+        </div>
+      </form>
     </div>
-  </div>
-  </>);
+  );
 };
