f24e9c51907607db57dce473db979af43b4c9e04
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-09-07 01:51:14 +0200

    validateEmailPage

---
+++ src/components/validateEmail/ValidateFormMidSection.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+export const ValidateFormMidSection = ({verificationCode,setVerificationCode}) => {
+  return (
+    <>
+    <div className="pb-2">
+					<label
+						htmlFor="verificationCode"
+						className="block mb-4 text-sm font-medium text-[#111827]"
+					>
+						Código de Verificación
+					</label>
+					<input
+						type="number"
+						name="verificationCode"
+						id="verificationCode"
+						className="mb-4 bg-[#ecf1ff] text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4"
+						placeholder="Ingrese el código de verificación"
+						value={verificationCode}
+						onChange={(e) => setVerificationCode(e.target.value)}
+					/>
+				</div>
+    </>
+  )
+}
+
+ValidateFormMidSection.propTypes = {
+  verificationCode: PropTypes.string.isRequired,
+  setVerificationCode: PropTypes.func.isRequired,
+};
---
+++ src/components/validateEmail/ValidationForm.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { notify } from "../../utils/notify";
+import { validateEmail } from "./validateEmailFetch";
+import { ValidateFormTopSection } from "./validateFormTopSection";
+import {ValidateFormMidSection} from './validateFormMidSection';
+
+
+export const VerificationForm = () => {
+	const [verificationCode, setVerificationCode] = useState("");
+	const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		setIsButtonDisabled(verificationCode === "");
+	}, [verificationCode]);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const { message } = await validateEmail(verificationCode);
+			notify(message);
+			setTimeout(() => {
+				navigate("/login");
+			}, 2000);
+		} catch (error) {
+			notify(error.message);
+		}
+	};
+
+	return (
+		<div className="flex flex-col w-full md:w-1/2 xl:w-2/5 2xl:w-2/5 3xl:w-1/3 mx-auto p-8 md:p-10 2xl:p-12 3xl:p-14 bg-[#ffffff] rounded-2xl shadow-xl mb-20 ">
+		<ValidateFormTopSection/>
+			<form className="flex flex-col" onSubmit={handleSubmit}>
+				<ValidateFormMidSection
+        verificationCode={verificationCode}
+        setVerificationCode={setVerificationCode}
+        />
+				<div className="flex flex-col items-center">
+					<button
+						type="submit"
+						className={`w-full text-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md p-4 text-center mb-6 ${
+							isButtonDisabled ? "bg-[#bdd0ff]" : "bg-[#628eff]"
+						}`}
+						disabled={isButtonDisabled}
+					>
+						Validar
+					</button>
+				</div>
+				<ToastContainer />
+			</form>
+		</div>
+	);
+};
