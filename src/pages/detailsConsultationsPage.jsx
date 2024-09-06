542ed78202cdb34db64fd8952e387d4b63d556e7
Ivan Sanchez <mourazos1995@gmail.com>
2024-09-06 18:19:08 +0200

    imputs-details

--- src/pages/detailsConsultationsPage.jsx
+++ src/pages/detailsConsultationsPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { useParams } from "react-router-dom";
+import { FiEdit } from "react-icons/fi";
 
 export const DetailsConsultationPage = () => {
   const { id } = useParams();
@@ -53,10 +54,13 @@ export const DetailsConsultationPage = () => {
         </div>
         <div className="w-6"></div>
       </div>
-      <header className="text-2xl font-bold mb-4">
+      <header className="text-2xl font-bold mb-4 relative w-full">
         Detalles de la consulta
       </header>
       <main className="bg-white p-6 rounded-lg shadow-md w-full max-w-4xl">
+        <button className="absolute right-1 mt-2 mr-2 p-2  text-[#628eff] rounded">
+          <FiEdit size={25} />
+        </button>
         <h2 className="text-lg font-semibold mb-2">Paciente:</h2>
         <div className="flex items-center mb-4">
           <img
@@ -64,7 +68,17 @@ export const DetailsConsultationPage = () => {
             src={consultationDetails.patientAvatar}
             alt="Avatar del paciente"
           />
-          <p className="text-lg">{consultationDetails.patientName}</p>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Nombre del Paciente
+            </label>
+            <input
+              type="text"
+              className="text-lg w-auto h-auto"
+              value={consultationDetails.patientName}
+              disabled
+            />
+          </div>
         </div>
         <h2 className="text-lg font-semibold mb-2">Doctor:</h2>
         <div className="flex items-center mb-4">
@@ -73,39 +87,96 @@ export const DetailsConsultationPage = () => {
             src={consultationDetails.doctorAvatar}
             alt="Avatar del doctor"
           />
-          <p className="text-lg">{consultationDetails.doctorName}</p>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Nombre del Doctor
+            </label>
+            <input
+              type="text"
+              className="text-lg w-auto h-auto"
+              value={consultationDetails.doctorName}
+              disabled
+            />
+          </div>
         </div>
         <h1 className="text-2xl font-bold text-[#4B5563] mb-4">
           {consultationDetails.title}
         </h1>
-        <p className="mb-2">
-          <strong>Descripción:</strong> {consultationDetails.description}
-        </p>
-        <p className="mb-2">
-          <strong>Severidad:</strong>
-          {consultationDetails.severity === "high"
-            ? " Alta"
-            : consultationDetails.severity === "medium"
-            ? " Media"
-            : consultationDetails.severity === "low"
-            ? " Baja"
-            : consultationDetails.severity}
-        </p>
-        <p className="mb-2">
-          <strong>Estado:</strong> {consultationDetails.status}
-        </p>
-        <p className="mb-2">
-          <strong>Fecha:</strong>{" "}
-          {new Date(consultationDetails.date).toLocaleString()}
-        </p>
-
-        <p className="mb-2">
-          <strong>Especialidad:</strong> {consultationDetails.specialityName}
-        </p>
-
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Descripción
+          </label>
+          <input
+            type="text"
+            className="mb-2 w-auto h-auto"
+            value={consultationDetails.description}
+            disabled
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Severidad
+          </label>
+          <input
+            type="text"
+            className="mb-2 w-auto h-auto"
+            value={
+              consultationDetails.severity === "high"
+                ? "Alta"
+                : consultationDetails.severity === "medium"
+                ? "Media"
+                : consultationDetails.severity === "low"
+                ? "Baja"
+                : consultationDetails.severity
+            }
+            disabled
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Estado
+          </label>
+          <input
+            type="text"
+            className="mb-2 w-auto h-auto"
+            value={consultationDetails.status}
+            disabled
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Fecha
+          </label>
+          <input
+            type="text"
+            className="mb-2 w-auto h-auto"
+            value={new Date(consultationDetails.date).toLocaleString()}
+            disabled
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Especialidad
+          </label>
+          <input
+            type="text"
+            className="mb-2 w-auto h-auto"
+            value={consultationDetails.specialityName}
+            disabled
+          />
+        </div>
         <h2 className="text-lg font-semibold mb-2">RESPUESTA:</h2>
-        <p className="mb-2">{consultationDetails.responseContent}</p>
-
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Contenido de la Respuesta
+          </label>
+          <input
+            type="text"
+            className="mb-2 w-auto h-auto"
+            value={consultationDetails.responseContent}
+            disabled
+          />
+        </div>
         <div className="mb-4">
           <h2 className="text-lg font-semibold mb-2">
             Archivos de la Consulta:
