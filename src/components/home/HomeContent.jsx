9506b240caa30de351cbce7bce9eaa344eae8095
DenisseBabio <denissebabiodoval@gmail.com>
2024-09-10 00:54:15 +0200

    Creando la Home page

---
+++ src/components/home/HomeContent.jsx
@@ -0,0 +1,10 @@
+
+import { HomeButtons } from './homeButtons';
+
+export const HomeContent = () => {
+    return (
+        <main className="flex flex-1 items-center justify-center flex-col bg-white p-6">
+            <HomeButtons />
+        </main>
+    );
+};
