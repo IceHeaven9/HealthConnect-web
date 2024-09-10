5a63ed1965a5461dc549f2350657d9f81feb29ae
DenisseBabio <denissebabiodoval@gmail.com>
2024-09-11 00:20:23 +0200

    eliminando el hamburguermenu y añadiendo botones necesarios en pantalla. También ajusto el HomeHeader

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -1,28 +1,19 @@
-import { HamburgerMenu } from '../HamburgerMenu';
-
 export const HomeHeader = () => {
     return (
-        <header className="flex items-center justify-between p-2 bg-[#cad6ff] text-white">
+        <header className="flex items-center justify-center p-2 bg-[#cad6ff] text-white h-20">
             {/* Logo y Nombre */}
             <div className="flex items-center space-x-4">
-                <div className="w-24 h-24">
+                <div className="w-20 h-20">
                     <img 
                         src="/images/Perfil_healthConnect-Photoroom.png" 
                         alt="Logo Preview" 
                         className="w-full h-full object-cover"
                     />
                 </div>
-                <div className="flex flex-col text-center">
-                    <div className="font-medium text-2xl text-[#628eff]">Health</div>
-                    <div className="font-medium text-2xl text-[#628eff]">Connect</div>
+                <div className="flex flex-col items-start">
+                    <div className="font-medium text-3xl text-[#628eff]">Health Connect</div>
                 </div>
-            </div>
-
-            {/* Hamburger Menu */}
-            <div>
-                <HamburgerMenu />
             </div>
         </header>
     );
 };
-
