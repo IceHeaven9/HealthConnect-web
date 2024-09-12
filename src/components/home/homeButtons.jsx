7814f55e606474824ecd35b8c2ed0398549634f9
DenisseBabio <denissebabiodoval@gmail.com>
2024-09-13 00:17:00 +0200

    modifico diseño, añado iconos a especialidades, texto y botones

--- src/components/home/homeButtons.jsx
+++ src/components/home/homeButtons.jsx
@@ -35,15 +35,39 @@ export const HomeButtons = () => {
               <FaUserPlus size={24} />
               Regístrate
             </Link>
-            <Link
-              className="flex items-center gap-2 text-sm bg-blue-700 rounded-lg text-white p-3 w-full max-w-xs"
-              to="/login"
-            >
-              <PiSignInLight size={24} /> Iniciar sesión
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
-  );
+
+            <div className="flex flex-col gap-4 items-center w-full">
+                 {/* Boton Cierre de sesion */}
+                {localStorage.getItem("TOKEN") ? (
+                    <button
+                        className="flex items-center gap-2 text-sm bg-red-500 rounded-lg text-white p-3 w-full max-w-xs"
+                        onClick={() => {
+                            localStorage.removeItem("TOKEN");
+                            window.location.href = "/login";
+                        }}
+                    >
+                        <CiLogout size={24} /> Cerrar sesión
+                    </button>
+                ) : (
+                    <>
+                     {/* Boton de Registro */}
+                        <Link
+                            className="flex items-center gap-2 text-sm bg-blue-700 rounded-lg text-white p-3 w-full max-w-xs"
+                            to="/register"
+                        >
+                            <FaUserPlus size={24} />
+                            Regístrate
+                        </Link>
+                         {/* Boton de inicio de sesión */}
+                        <Link
+                            className="flex items-center gap-2 text-sm bg-blue-700 rounded-lg text-white p-3 w-full max-w-xs"
+                            to="/login"
+                        >
+                            <PiSignInLight size={24} /> Iniciar sesión
+                        </Link>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 };
