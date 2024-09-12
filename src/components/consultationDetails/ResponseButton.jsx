3ce045863a8374df5b3ab3367388cf98b6ecf62d
kimberly feijoo <kimberlyfeijoo26@gmail.com>
2024-09-12 22:24:44 +0200

    Fetch funcionando, añade valoracion

--- src/components/consultationDetails/ResponseButton.jsx
+++ src/components/consultationDetails/ResponseButton.jsx
@@ -6,9 +6,8 @@ import { useState } from "react";
 import { customStyles } from "../../constants";
 import { sendRating } from "./fetch/sendRating";
 
-
-// Componente de estrellas para calificación
- export const StarRating = ({ rating, onRate }) => {
+// COMPONENTE DE ESTRELLAS
+export const StarRating = ({ rating, onRate }) => {
   const [hoverRating, setHoverRating] = useState(0);
 
   return (
@@ -27,103 +26,92 @@ import { sendRating } from "./fetch/sendRating";
     </div>
   );
 };
+//FIN DE COMPONENTE
 
-// Fin de componente de Estrella
 
 export const ResponseButton = ({
-    showResponseFiles,
-    setShowResponseFiles,
-    consultationDetails,
-    
+  showResponseFiles,
+  setShowResponseFiles,
+  consultationDetails,
 }) => {
-    
-     // Establecer rating
-   const [rating, setRating] = useState(0);
-  // Fin de establecer rating
+  const [rating, setRating] = useState(0);
 
+  const isDisabled =
+    !consultationDetails.responseContent &&
+    (!consultationDetails.responseFiles || consultationDetails.responseFiles.length === 0);
 
-    const isDisabled =
-        !consultationDetails.responseContent &&
-        (!consultationDetails.responseFiles || consultationDetails.responseFiles.length === 0);
+  const handleRating = async (newRating) => {
+    const ratingInt = parseInt(newRating, 10); // Convertimos a un entero
+   
+    try {
+      await sendRating(consultationDetails.id, ratingInt);
+      setRating(ratingInt);
+    } catch (error) {
+      console.error('Error al enviar la calificación', error);
+    }
+  };
 
-        const handleRating = async (newRating) => {
-            if (!consultationDetails.id) {  
-              console.error('Error: consultationId no está definido');
-              return;
-            }
-          
-            try {
-              await sendRating(consultationDetails.id, newRating);
-              setRating(newRating);
-            } catch (error) {
-              console.error('Error al enviar la calificación', error);
-            }
-          };
-
-    return (
-        <>
-            <button
-                className={`mb-4 p-2 w-full text-end text-smokeWhite rounded-lg flex flex-col items-center font-medium ${
-                    isDisabled ? "bg-darkBlue" : "bg-lightBlue"
-                }`}
-                onClick={() => setShowResponseFiles(!showResponseFiles)}
-                disabled={isDisabled}
-            >
-                {showResponseFiles ? <IoMdClose /> : <FaLaptopMedical size={30} />}{" "}
-                Respuesta
-            </button>
-            <Modal
-                isOpen={showResponseFiles}
-                onRequestClose={() => setShowResponseFiles(false)}
-                contentLabel="Response Files"
-                style={customStyles}
-            >
-                <button
-                    className="text-2xl"
-                    onClick={() => setShowResponseFiles(false)}
-                >
-                    <IoMdClose />
-                </button>
-                <div>
-                    <h2 className="text-xl font-semibold mb-2">RESPUESTA:</h2>
-                    <div>
-                        <p className="mb-2 w-full h-auto break-words">
-                            {consultationDetails.responseContent}
-                        </p>
-                        <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-                            <h3 className="text-xl font-semibold mb-2">Archivos:</h3>
-                            {consultationDetails.responseFiles.map((file, index) => (
-                                <div key={index} className="mb-2">
-                                    <img
-                                        className="w-full h-auto"
-                                        src={file.filePath}
-                                        alt={file.fileName}
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    {/* Aquí añadimos el sistema de calificación */}
-          <StarRating rating={rating} onRate={handleRating} />
-
-
-
+  return (
+    <>
+      <button
+        className={`mb-4 p-2 w-full text-end text-smokeWhite rounded-lg flex flex-col items-center font-medium ${
+          isDisabled ? "bg-darkBlue" : "bg-lightBlue"
+        }`}
+        onClick={() => setShowResponseFiles(!showResponseFiles)}
+        disabled={isDisabled}
+      >
+        {showResponseFiles ? <IoMdClose /> : <FaLaptopMedical size={30} />}{" "}
+        Respuesta
+      </button>
+      <Modal
+        isOpen={showResponseFiles}
+        onRequestClose={() => setShowResponseFiles(false)}
+        contentLabel="Response Files"
+        style={customStyles}
+      >
+        <button
+          className="text-2xl"
+          onClick={() => setShowResponseFiles(false)}
+        >
+          <IoMdClose />
+        </button>
+        <div>
+          <h2 className="text-xl font-semibold mb-2">RESPUESTA:</h2>
+          <div>
+            <p className="mb-2 w-full h-auto break-words">
+              {consultationDetails.responseContent}
+            </p>
+            <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+              <h3 className="text-xl font-semibold mb-2">Archivos:</h3>
+              {consultationDetails.responseFiles.map((file, index) => (
+                <div key={index} className="mb-2">
+                  <img
+                    className="w-full h-auto"
+                    src={file.filePath}
+                    alt={file.fileName}
+                  />
                 </div>
-            </Modal>
-        </>
-    );
+              ))}
+            </div>
+          </div>
+          {/* Aquí añadimos el sistema de calificación componente estrella */}
+          <StarRating rating={rating} onRate={handleRating} />
+        </div>
+      </Modal>
+    </>
+  );
 };
 
 ResponseButton.propTypes = {
-    showResponseFiles: PropTypes.bool.isRequired,
-    setShowResponseFiles: PropTypes.func.isRequired,
-    consultationDetails: PropTypes.shape({
-        responseContent: PropTypes.string,
-        responseFiles: PropTypes.arrayOf(
-            PropTypes.shape({
-                filePath: PropTypes.string,
-                fileName: PropTypes.string,
-            })
-        ).isRequired,
-    }).isRequired,
+  showResponseFiles: PropTypes.bool.isRequired,
+  setShowResponseFiles: PropTypes.func.isRequired,
+  consultationDetails: PropTypes.shape({
+    responseContent: PropTypes.string,
+    responseFiles: PropTypes.arrayOf(
+      PropTypes.shape({
+        filePath: PropTypes.string,
+        fileName: PropTypes.string,
+      })
+    ).isRequired,
+  }).isRequired,
 };
