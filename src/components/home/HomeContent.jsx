e5605863b835b6bdbc58b56b706dcce57f3e8f1d
DenisseBabio <denissebabiodoval@gmail.com>
2024-09-15 22:37:05 +0200

    responsivo

--- src/components/home/HomeContent.jsx
+++ src/components/home/HomeContent.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { HomeButtons } from "./homeButtons";
+import { API_HOST } from "../../constants";
 
 {
-  /* Iconos especialidades */
+  /*Iconos especialidades*/
 }
 const specialtyIcons = [
   { id: 1, icon: "cardiologia.png" },
@@ -32,7 +33,7 @@ export const HomeContent = () => {
   const [doctors, setDoctors] = useState([]);
 
   {
-    /* Funcion para iconos especialidades */
+    /*Función para obtener los iconos de especialidades */
   }
   const getSpecialtyIcon = (specialtyId) => {
     const icon = specialtyIcons.find(
@@ -44,11 +45,11 @@ export const HomeContent = () => {
   };
 
   {
-    /* Funcion para especialidades desde el back */
+    /*Función para obtener especialidades desde el backend */
   }
   const fetchSpecialties = async () => {
     try {
-      const response = await fetch("http://localhost:3000/specialities");
+      const response = await fetch(`${API_HOST}/specialities`);
       const result = await response.json();
       setSpecialties(result);
     } catch (error) {
@@ -57,11 +58,11 @@ export const HomeContent = () => {
   };
 
   {
-    /* Funcion para doctores desde el back */
+    /* Función para obtener doctores desde el backend*/
   }
   const fetchDoctors = async () => {
     try {
-      const response = await fetch("http://localhost:3000/doctors");
+      const response = await fetch(`${API_HOST}/doctors`);
       const result = await response.json();
       setDoctors(result);
     } catch (error) {
@@ -77,35 +78,40 @@ export const HomeContent = () => {
   return (
     <main className="flex flex-col items-center bg-white p-4 gap-4 min-h-screen">
       {/* Texto */}
-      <div className="text-center mb-6 max-w-4xl bg-blue-100 text-blue-600 p-6 rounded-lg shadow-md">
-        <h1 className="text-2xl md:text-3xl font-bold mb-4">
-          Tu salud simplificada.
-        </h1>
-        <p className="text-base md:text-lg leading-relaxed">
-          Hacemos que sea fácil reservar citas con los mejores profesionales de
-          la salud.
-        </p>
-        <ul className="list-disc list-inside text-base md:text-lg leading-relaxed">
-          <li>
-            <span className="font-semibold">Atención experta:</span> conéctese
-            con médicos calificados de diversas especialidades.
-          </li>
-          <li>
-            <span className="font-semibold">Reserva fácil:</span> programe sus
-            citas en línea con solo unos pocos clics.
-          </li>
-          <li>
-            <span className="font-semibold">Sin esperas:</span> elija el horario
-            que mejor se adapte a sus necesidades.
-          </li>
-          <li>
-            <span className="font-semibold">Seguro y Privado:</span> su
-            información médica está protegida con nosotros.
-          </li>
-        </ul>
-        <p className="text-base md:text-lg leading-relaxed mt-6">
-          ¡Únase hoy y tome el control de su salud con facilidad!
-        </p>
+      <div
+        className="text-center mb-6 max-w-4xl bg-cover bg-center p-6 rounded-lg shadow-md"
+        style={{ backgroundImage: "url('/images/fondotext.jpg')" }}
+      >
+        <div className="bg-white bg-opacity-80 p-6 rounded-lg">
+          <h1 className="text-2xl md:text-3xl font-bold mb-4 text-blue-600">
+            Tu salud simplificada.
+          </h1>
+          <p className="text-base md:text-lg leading-relaxed mb-6 text-blue-600">
+            Hacemos que sea fácil reservar citas con los mejores profesionales
+            de la salud.
+          </p>
+          <ul className="list-disc list-inside text-base md:text-lg leading-relaxed mb-6 text-blue-600">
+            <li>
+              <span className="font-semibold">Atención experta:</span> conéctese
+              con médicos calificados de diversas especialidades.
+            </li>
+            <li>
+              <span className="font-semibold">Reserva fácil:</span> programe sus
+              citas en línea con solo unos pocos clics.
+            </li>
+            <li>
+              <span className="font-semibold">Sin esperas:</span> elija el
+              horario que mejor se adapte a sus necesidades.
+            </li>
+            <li>
+              <span className="font-semibold">Seguro y Privado:</span> su
+              información médica está protegida con nosotros.
+            </li>
+          </ul>
+          <p className="text-base md:text-lg leading-relaxed text-blue-600">
+            ¡Únase hoy y tome el control de su salud con facilidad!
+          </p>
+        </div>
       </div>
 
       {/* Botones */}
@@ -113,10 +119,11 @@ export const HomeContent = () => {
         <HomeButtons />
       </div>
 
-      {/* Contenedor especialidades */}
+      {/* Contenedor especialidades y doctores */}
       <div className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="bg-blue-200 p-4 rounded-lg shadow-lg overflow-auto max-h-96">
-          <h2 className="text-xl font-bold mb-4 text-blue-800">
+        {/* Especialidades */}
+        <div className="bg-blue-200 px-4 pb-4 rounded-lg shadow-lg overflow-auto max-h-96">
+          <h2 className="text-xl font-bold mb-4 text-blue-800 sticky top-0 bg-blue-200 p-2">
             Especialidades
           </h2>
           {specialties.length > 0 ? (
@@ -126,7 +133,7 @@ export const HomeContent = () => {
                   <img
                     src={getSpecialtyIcon(specialty.id)}
                     alt={specialty.name}
-                    className="w-8 h-8 mr-4"
+                    className="w-12 h-12 mr-4"
                     onError={(e) => {
                       e.target.src = "/images/specialtyIcons/default.png";
                       console.error("Error al cargar la imagen:", e);
@@ -141,15 +148,17 @@ export const HomeContent = () => {
           )}
         </div>
 
-        {/* Contenedor doctores */}
-        <div className="bg-blue-200 p-4 rounded-lg shadow-lg overflow-auto max-h-96">
-          <h2 className="text-xl font-bold mb-4 text-blue-800">Doctores</h2>
+        {/* Doctores */}
+        <div className="bg-blue-200 px-4 pb-4 rounded-lg shadow-lg overflow-auto max-h-96">
+          <h2 className="text-xl font-bold mb-4 text-blue-800 sticky top-0 bg-blue-200 p-2">
+            Doctores
+          </h2>
           {doctors.length > 0 ? (
             <ul className="space-y-3">
               {doctors.map((doctor) => (
                 <li key={doctor.id} className="flex items-center">
                   <img
-                    src={doctor.avatar}
+                    src={doctor.avatar || "/images/doctors/default-avatar.png"}
                     alt={`${doctor.firstName} ${doctor.lastName}`}
                     className="w-12 h-12 rounded-full mr-4"
                     onError={(e) => {
