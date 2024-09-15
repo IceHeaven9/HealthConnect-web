fc470960bbe2caf2090b54911bbc31b03c5d14cc
DenisseBabio <denissebabiodoval@gmail.com>
2024-09-15 22:19:19 +0200

    elimino boton de crear consulta en pantalla

--- src/components/home/homeButtons.jsx
+++ src/components/home/homeButtons.jsx
@@ -1,18 +1,13 @@
 import { Link } from "react-router-dom";
-import { CiLogout } from "react-icons/ci"; // Asegúrate de tener esta importación
-import { FaUserPlus } from "react-icons/fa6"; // Importa el ícono necesario
-import { PiSignInLight } from "react-icons/pi"; // Importa el ícono necesario
+import { CiLogout } from "react-icons/ci";
+import { FaUserPlus } from "react-icons/fa6";
+import { PiSignInLight } from "react-icons/pi";
 
 export const HomeButtons = () => {
     return (
         <div className="flex flex-col gap-4 items-center w-full">
-            {/* Botón Crear Consulta */}
-            <Link to="/create-consultation" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
-                Crear Consulta
-            </Link>
-
             <div className="flex flex-col gap-4 items-center w-full">
-                 {/* Boton Cierre de sesion */}
+                {/* Botón Cierre de sesión */}
                 {localStorage.getItem("TOKEN") ? (
                     <button
                         className="flex items-center gap-2 text-sm bg-red-500 rounded-lg text-white p-3 w-full max-w-xs"
@@ -25,7 +20,7 @@ export const HomeButtons = () => {
                     </button>
                 ) : (
                     <>
-                     {/* Boton de Registro */}
+                        {/* Botón de Registro */}
                         <Link
                             className="flex items-center gap-2 text-sm bg-blue-700 rounded-lg text-white p-3 w-full max-w-xs"
                             to="/register"
@@ -33,7 +28,7 @@ export const HomeButtons = () => {
                             <FaUserPlus size={24} />
                             Regístrate
                         </Link>
-                         {/* Boton de inicio de sesión */}
+                        {/* Botón de inicio de sesión */}
                         <Link
                             className="flex items-center gap-2 text-sm bg-blue-700 rounded-lg text-white p-3 w-full max-w-xs"
                             to="/login"
@@ -46,4 +41,3 @@ export const HomeButtons = () => {
         </div>
     );
 };
-
