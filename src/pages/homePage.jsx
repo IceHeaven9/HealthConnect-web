3a8c82d31797c6c2121890fd225cdf5cea73b468
Ivan Sanchez <mourazos1995@gmail.com>
2024-09-16 18:56:17 +0200

    rebase-main

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,13 +1,11 @@
-import { HomeHeader } from '../components/home/homeHeader';
-import { HomeContent } from '../components/home/homeContent';
-import { Footer } from '../components/Footer';
+import { HomeHeader } from "../components/home/homeHeader";
+import { HomeContent } from "../components/home/homeContent";
 
 export const HomePage = () => {
-    return (
-        <>
-            <HomeHeader />
-            <HomeContent />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <HomeHeader />
+      <HomeContent />
+    </>
+  );
 };
